079dac319724d0e5e6c8d580abab5b7bb88d6f29
Kevin Wolbart <kevinwolbart@gmail.com>
2023-11-07 15:41:37 -0600

    Added logic to check for wins and ties

--- main.js
+++ main.js
@@ -39,7 +39,38 @@ const Gameboard = (function () {
         return ans;
     }
 
-    return {displayBoard, setBoard, isEmpty}
+    function inARow() {
+        if (board[0] == board[1] && board[0] == board[2]) {
+            return board[0];
+        } else if (board[3] == board[4] && board[3] == board[5]) {
+            return board[3];
+        } else if (board[6] == board[7] && board[6] == board[8]) {
+            return board[6];
+        } else if (board[0] == board[3] && board[0] == board[6]) {
+            return board[0];
+        } else if (board[1] == board[4] && board[1] == board[7]) {
+            return board[1];
+        } else if (board[2] == board[5] && board[2] == board[8]) {
+            return board[2];
+        } else if (board[0] == board[4] && board[0] == board[8]) {
+            return board[0];
+        } else if (board[2] == board[4] && board[2] == board[6]) {
+            return board[2];
+        }
+
+        return false;
+    }
+
+    function isFull() {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return {displayBoard, setBoard, isEmpty, inARow, isFull};
 })();
 
 const Game = (function () {
@@ -61,8 +92,34 @@ const Game = (function () {
         if (Gameboard.isEmpty(e.target.dataset.square)) {
             Gameboard.setBoard(e.target.dataset.square);
             Gameboard.displayBoard();
+            isGameOver();
             switchTurn();
             console.log(e.target);
+        }
+    }
+
+    function checkWin() {
+        if (Gameboard.inARow()) {
+            //Check if there is a winner and return which player won
+            console.log('Winner');
+            return true;
+        }
+
+        return false;
+    }
+
+    function checkTie() {
+        if (Gameboard.isFull()) {
+            return true;
+        }
+        return false;
+    }
+
+    function isGameOver() {
+        if (checkWin()) {
+            console.log('Winner');
+        } else if (checkTie()) {
+            console.log('Tie Game');
         }
     }
 
