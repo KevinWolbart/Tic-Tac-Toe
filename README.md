c57ca3b4f0188720410ae8385750c430fab9a9e4
Kevin Wolbart <kevinwolbart@gmail.com>
2023-11-08 17:04:43 -0600

    Added player interface full game play cycle done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tic-Tac-Toe
 Odin Project Tic Tac Toe Game
+
+
+Notes for revisit:
+- After game is finish add Play Again button 
+- Create Win counter
+- Change reset button to completely reset game, names and all and make board unclickable until new names are entered
+- Make board unclickable after win
--- main.js
+++ main.js
@@ -70,7 +70,22 @@ const Gameboard = (function () {
         return true;
     }
 
-    return {displayBoard, setBoard, isEmpty, inARow, isFull};
+    function resetBoard() {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] !== 0) {
+                board[i] = 0;
+            }
+            let block = document.getElementById(i);
+            if (block.hasChildNodes()) {
+                block.firstChild;
+                block.removeChild(block.lastChild);
+            }
+        }
+        displayBoard();
+        Game.resetTurn();
+    }
+
+    return {displayBoard, setBoard, isEmpty, inARow, isFull, resetBoard};
 })();
 
 const Game = (function () {
@@ -88,6 +103,10 @@ const Game = (function () {
         }
     }
 
+    function resetTurn() {
+        turn = 1;
+    }
+
     function playMove(e) {
         if (Gameboard.isEmpty(e.target.dataset.square)) {
             Gameboard.setBoard(e.target.dataset.square);
@@ -100,9 +119,11 @@ const Game = (function () {
 
     function checkWin() {
         if (Gameboard.inARow()) {
-            //Check if there is a winner and return which player won
-            console.log('Winner');
-            return true;
+            if (Gameboard.inARow() == 1) {
+                return "Player 1";
+            } else {
+                return "Player 2";
+            }
         }
 
         return false;
@@ -110,63 +131,119 @@ const Game = (function () {
 
     function checkTie() {
         if (Gameboard.isFull()) {
-            return true;
+            return "Tie";
         }
         return false;
     }
 
     function isGameOver() {
         if (checkWin()) {
-            console.log('Winner');
+            DisplayController.displayResult(checkWin());
         } else if (checkTie()) {
-            console.log('Tie Game');
+            DisplayController.displayResult(checkTie());
         }
     }
 
-    return {switchTurn, playMove, getTurn};
+    return {switchTurn, playMove, getTurn, resetTurn};
 })();
 
 const DisplayController = (function () {
-    const zero = document.getElementById('0');
-    const one = document.getElementById('1');
-    const two = document.getElementById('2');
-    const three = document.getElementById('3');
-    const four = document.getElementById('4');
-    const five = document.getElementById('5');
-    const six = document.getElementById('6');
-    const seven = document.getElementById('7');
-    const eight = document.getElementById('8');
-
-    zero.dataset.square = 0;
-    one.dataset.square = 1;
-    two.dataset.square = 2;
-    three.dataset.square = 3;
-    four.dataset.square = 4;
-    five.dataset.square = 5;
-    six.dataset.square = 6;
-    seven.dataset.square = 7;
-    eight.dataset.square = 8;
-
     
 
-    zero.addEventListener('click', Game.playMove);
-    one.addEventListener('click', Game.playMove);
-    two.addEventListener('click', Game.playMove);
-    three.addEventListener('click', Game.playMove);
-    four.addEventListener('click', Game.playMove);
-    five.addEventListener('click', Game.playMove);
-    six.addEventListener('click', Game.playMove);
-    seven.addEventListener('click', Game.playMove);
-    eight.addEventListener('click', Game.playMove);
+    const startRestart = document.getElementById('start-restart');
+    const player1name = document.getElementById('player-1-name');
+    const player2name = document.getElementById('player-2-name');
+    const player1textbox = document.getElementById('player-1-textbox');
+    const player2textbox = document.getElementById('player-2-textbox');
+    const output = document.getElementById('player-2-output');
+
+    const player1 = createPlayer('player1');
+    const player2 = createPlayer('player2');
+
+    startRestart.addEventListener('click', function() {
+        //On first click initalize board and event listeners for the squares and toggle the start button to restart button. Additionally remove the name entry box replace h4 with player names.
+        //On click of restart, reset the board.
+        if (!startRestart.classList.contains('restart')) {
+
+            if (player1textbox.value && player2textbox.value) {
+                const zero = document.getElementById('0');
+                const one = document.getElementById('1');
+                const two = document.getElementById('2');
+                const three = document.getElementById('3');
+                const four = document.getElementById('4');
+                const five = document.getElementById('5');
+                const six = document.getElementById('6');
+                const seven = document.getElementById('7');
+                const eight = document.getElementById('8');
+
+                zero.dataset.square = 0;
+                one.dataset.square = 1;
+                two.dataset.square = 2;
+                three.dataset.square = 3;
+                four.dataset.square = 4;
+                five.dataset.square = 5;
+                six.dataset.square = 6;
+                seven.dataset.square = 7;
+                eight.dataset.square = 8;
+
+                zero.addEventListener('click', Game.playMove);
+                one.addEventListener('click', Game.playMove);
+                two.addEventListener('click', Game.playMove);
+                three.addEventListener('click', Game.playMove);
+                four.addEventListener('click', Game.playMove);
+                five.addEventListener('click', Game.playMove);
+                six.addEventListener('click', Game.playMove);
+                seven.addEventListener('click', Game.playMove);
+                eight.addEventListener('click', Game.playMove);
+
+                startRestart.classList.toggle('restart');
+                startRestart.textContent = 'Restart';
+
+                player1.setName(player1textbox.value);
+                player2.setName(player2textbox.value);
+
+                player1textbox.classList.add('hidden');
+                player2textbox.classList.add('hidden');
+
+                player1name.classList.add('named');
+                player2name.classList.add('named');
+
+                player1name.textContent = player1.getName();
+                player2name.textContent = player2.getName();
+
+
+            } else {
+                alert('Both players must have names to start.');
+            }
+        } else {
+            Gameboard.resetBoard();
+            if (output.textContent) {
+                output.textContent = "";
+            }
+        }
+
+    })
+
+    function displayResult(result) {
+        if (result == "Player 1") {
+            output.textContent = `${player1.getName()} won the game!`;
+        } else if(result == "Player 2") {
+            output.textContent = `${player2.getName()} won the game!`;
+        } else if (result == "Tie") {
+            output.textContent = "It's a tie!";
+        }
+    }
+
+    return {displayResult};
 
 })();
 
 function createPlayer(name) {
-    const getName = () => name;
+    let playerName = name;
 
-    return { getName };
+    const getName = () => {return playerName;}
+
+    const setName = (newName) => playerName = newName;
+
+    return { getName, setName };
 }
-
-//DisplayController.watchClick();
-//Game.playMove(0);
-//Gameboard.displayBoard();
